e1d1e75befb898018a63da041a4a0029af3f03ff
lucasestrade <43579718+lucasestrade@users.noreply.github.com>
2021-03-18 14:49:32 +0100

    update routing + example

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,37 +1,19 @@
 import React, { useState } from 'react';
 import { View } from 'react-native';
-// import useApp from "../hooks/useApp";
-// import { RefreshViewScroll } from "../components/RefreshView";
 import MyGroupsEventsCard from '../components/cards/MyGroupsEventsCard';
 import { GroupsProvider } from "../context/groupsContext";
 import { EventsProvider } from "../context/eventsContext";
-import { Button } from 'react-native';
 
 export default function HomeScreen({navigation}) {
 
-    // const { actions } = useApp();
-    // const [a, setA] = useState([]);
-    // console.log(a);
-
-    // const b = async () => {
-    //   let res = await actions.fetchUserParameters();
-    //   setA(res);
-    // }
-
     return (
-      // <RefreshViewScroll onRefresh={() => b()}>
       <EventsProvider>
         <GroupsProvider>
           <View>
             <MyGroupsEventsCard navigation={navigation} type="groups"/>
             <MyGroupsEventsCard navigation={navigation} type="events"/>
-            <Button 
-            title="TESSSST"
-              onPress={() => navigation.navigate("test")}
-            />
           </View>
         </GroupsProvider>
       </EventsProvider>
-      // </RefreshViewScroll>
     );
 }
--- screens/TestScreen.js
+++ screens/TestScreen.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Text, View, Button } from 'react-native';
-
+import useApp from "../hooks/useApp";
+import { RefreshViewScroll } from "../components/RefreshView";
 
 export default function TestScreen({navigation}) {
+
+    const { actions } = useApp();
+    const [a, setA] = useState([]);
+    console.log(a);
+
+    const b = async () => {
+      let res = await actions.fetchUserParameters();
+      setA(res);
+    }
+
+    useEffect(() => {
+      navigation.setOptions({
+        headerTitle: "ta mere",
+      });
+    });
+
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Teeeest</Text>
-        <Button 
-            title="TESSSST"
-              onPress={() => navigation.navigate("MyGroups")}
-            />
-      </View>
+      <RefreshViewScroll onRefresh={() => b()}>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Text>Teeeest</Text>
+          <Button 
+              title="TESSSST"
+                onPress={() => navigation.navigate("MyGroups")}
+              />
+        </View>
+      </RefreshViewScroll>
     );
   }
