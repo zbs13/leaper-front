03d436487828c003df0980d0c96d6cbe8976587b
Lucas Estrade <lucas.estrade5@hotmail.fr>
2021-03-22 20:20:39 +0100

    fix folder name

--- components/Menus/LeftToggleMenu.js
+++ components/Menus/LeftToggleMenu.js
@@ -10,30 +10,30 @@ const Drawer = createDrawerNavigator();
 
 export default function LeftToggleMenu() {
 
-    const {selectors} = useApp();
+    const { selectors } = useApp();
 
     return (
         <Drawer.Navigator drawerStyle={{ backgroundColor: '#ffff', width: 260 }}>
             {
                 global.listSports(selectors.getLang()).map((value, index) => {
                     return (
-                        <Drawer.Screen 
+                        <Drawer.Screen
                             key={index}
-                            name= {value.name}
-                            component={typeof value.component === "undefined" ? SportEventScreenManager : value.component === global.screens.HOME ? BottomMenu : null} 
+                            name={value.name}
+                            component={typeof value.component === "undefined" ? SportEventScreenManager : value.component === global.screens.HOME ? BottomMenu : null}
                             options={{
-                    
+
                                 title: value.name,
-                                drawerIcon: ({focused, size}) => (
+                                drawerIcon: ({ focused, size }) => (
                                     <Ionicons
-                                        name= {value.icon}
+                                        name={value.icon}
                                         size={size}
                                         color={focused ? '#BDE023' : '#ccc'}
                                     />
                                 ),
                             }}
                         />
-                    )  
+                    )
                 })
             }
         </Drawer.Navigator>
